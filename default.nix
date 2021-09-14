83da5b77c611a94e1e871853cd7fb20791bb5006
Lan Tian <xuyh0120@outlook.com>
2021-09-14 02:12:58 -0500

    example-package: remove

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   liboqs = pkgs.callPackage ./pkgs/liboqs { };
   boringssl-oqs = pkgs.callPackage ./pkgs/boringssl-oqs { };
 }
