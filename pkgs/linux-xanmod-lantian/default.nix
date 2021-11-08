e5f6f99a092c8be46c12249be76733e3700b115b
Lan Tian <xuyh0120@outlook.com>
2021-11-08 01:28:25 -0600

    Revert "linux-xanmod-lantian: remove unneeded config.nix"
    This reverts commit 49ef5ac80312ee82029069ff53d76cd52006da75.

--- pkgs/linux-xanmod-lantian/default.nix
+++ pkgs/linux-xanmod-lantian/default.nix
@@ -15,19 +15,6 @@ linuxManualConfig rec {
 
   modDirVersion = "${linuxKernel.kernels.linux_xanmod.modDirVersion}-lantian";
   configfile = ./config;
-  config = stdenv.mkDerivation {
-    name = "linux-xanmod-lantian.config";
-    src = ./config;
-    phases = [ "buildPhase" ];
-    buildPhase = ''
-      echo "{" > "$out"
-      while IFS='=' read key val; do
-        [ "x''${key#CONFIG_}" != "x$key" ] || continue
-        no_firstquote="''${val#\"}";
-        echo '  "'"$key"'" = "'"''${no_firstquote%\"}"'";' >> "$out"
-      done < "${configfile}"
-      echo "}" >> $out
-    '';
-  };
+  config = import ./config.nix;
   allowImportFromDerivation = true;
 }
