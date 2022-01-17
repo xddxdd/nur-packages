cc411e4a6dce0de048bfde7980b5872929f048e0
Lan Tian <xuyh0120@outlook.com>
2022-01-16 21:29:20 -0600

    openresty-lantian: remove unstable http3 support

--- default.nix
+++ default.nix
@@ -44,7 +44,7 @@ rec {
   };
   onepush = pkgs.callPackage ./pkgs/onepush { };
   openresty-lantian = pkgs.callPackage ./pkgs/openresty-lantian {
-    inherit liboqs boringssl-oqs;
+    inherit liboqs openssl-oqs;
   };
   openssl-oqs = pkgs.callPackage ./pkgs/openssl-oqs {
     inherit liboqs;
