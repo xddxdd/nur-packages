a1a6208fc9f17bcd855a486867847ef3c5982b2f
Lan Tian <xuyh0120@outlook.com>
2022-03-20 23:49:17 -0500

    baidunetdisk: try original binary, doesn't work

--- pkgs/baidunetdisk/default.nix
+++ pkgs/baidunetdisk/default.nix
@@ -3,6 +3,7 @@
 , writeShellScript
 , writeShellScriptBin
 , autoPatchelfHook
+, makeWrapper
 , patchelf
 , steam
 , lib
@@ -14,60 +15,68 @@ let
   version = "4.3.0";
 
   libraries = with pkgs; [
-    libva
-    stdenv.cc.cc.lib
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    cairo
+    cups
+    dbus
+    expat
+    gdk-pixbuf
+    glib
+    gtk3
+    libpulseaudio
+    mesa_drivers
+    nspr
+    nss
+    pango
     pciutils
+    udev
+    xorg.libX11
+    xorg.libxcb
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXScrnSaver
+    xorg.libxshmfence
+    xorg.libXtst
   ];
-
-  resource = stdenv.mkDerivation rec {
-    pname = "baidunetdisk-resource";
-    inherit version;
-    src = fetchurl {
-      url = "http://wppkg.baidupcs.com/issue/netdisk/Linuxguanjia/${version}/baidunetdisk_${version}_amd64.deb";
-      sha256 = "sha256-Q6oK1O+P4X+mJZKjZr2VWYTXHlYyC1p7OrKu4LSb5eY=";
-    };
-
-    nativeBuildInputs = [ autoPatchelfHook patchelf ];
-    buildInputs = [ stdenv.cc.cc.lib ];
-
-    unpackPhase = ''
-      ar x ${src}
-    '';
-
-    installPhase = ''
-      mkdir -p $out
-      tar xf data.tar.xz -C $out
-      mv $out/usr/* $out/
-      rm -rf $out/usr
-
-      mkdir $out/lib
-      mv $out/opt/baidunetdisk/libbrowserengine.so $out/lib/
-      mv $out/opt/baidunetdisk/libkernel.so $out/lib/
-      mv $out/opt/baidunetdisk/libminosagent.so $out/lib/
-      mv $out/opt/baidunetdisk/libplayer.so $out/lib/
-
-      # Workaround missing symbol pthread_mutexattr_init
-      for F in $out/lib/*.so; do
-        patchelf --add-needed libpthread.so.0 $F
-      done
-
-      mv $out/opt/baidunetdisk/resources $out/
-      rm -rf $out/opt
-
-      find $out/resources/ -maxdepth 1 -type f \
-        -not -name app.asar\* \
-        -not -name version \
-        -exec rm {} \;
-    '';
+in
+stdenv.mkDerivation rec {
+  pname = "baidunetdisk-resource";
+  inherit version;
+  src = fetchurl {
+    url = "http://wppkg.baidupcs.com/issue/netdisk/Linuxguanjia/${version}/baidunetdisk_${version}_amd64.deb";
+    sha256 = "sha256-Q6oK1O+P4X+mJZKjZr2VWYTXHlYyC1p7OrKu4LSb5eY=";
   };
 
-  startScript = writeShellScriptBin "baidunetdisk" ''
-    export LD_PRELOAD="${resource}/lib/libbrowserengine.so:${resource}/lib/libkernel.so"
-    export LD_LIBRARY_PATH="${lib.makeLibraryPath libraries}:${resource}/lib:''${LD_LIBRARY_PATH}"
-    ${pkgs.electron}/bin/electron \
-      --no-sandbox \
-      ${resource}/resources/app.asar
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+  buildInputs = [ stdenv.cc.cc.lib ] ++ libraries;
+
+  unpackPhase = ''
+    ar x ${src}
   '';
-in
-# resource
-startScript
+
+  installPhase = ''
+    mkdir -p $out
+    tar xf data.tar.xz -C $out
+    mv $out/usr/* $out/
+    rm -rf $out/usr
+
+    # Workaround missing symbol pthread_mutexattr_init
+    for F in $out/opt/baidunetdisk/*.so; do
+      patchelf --add-needed libpthread.so.0 $F
+    done
+
+    mkdir -p $out/bin
+    makeWrapper $out/opt/baidunetdisk/baidunetdisk $out/bin/baidunetdisk \
+      --argv0 "baidunetdisk" \
+      --prefix LD_PRELOAD : "$out/opt/baidunetdisk/libbrowserengine.so:$out/opt/baidunetdisk/libkernel.so" \
+      --prefix LD_LIBRARY_PATH : "$out/opt/baidunetdisk:${lib.makeLibraryPath libraries}"
+  '';
+}
