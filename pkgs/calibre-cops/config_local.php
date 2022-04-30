8a5057d2e66bdea4fefb132d5f87184d0d5785d7
Lan Tian <xuyh0120@outlook.com>
2022-04-29 21:22:41 -0500

    calibre-cops: add

---
+++ pkgs/calibre-cops/config_local.php
@@ -0,0 +1,2 @@
+<?php
+require("/etc/calibre-cops/config_local.php");
---
+++ pkgs/calibre-cops/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, stdenv
+, fetchurl
+, unzip
+, ...
+} @ args:
+
+let
+  configFile = ./config_local.php;
+in
+stdenv.mkDerivation rec {
+  pname = "calibre-cops";
+  version = "1.1.3";
+  src = fetchurl {
+    url = "https://github.com/seblucas/cops/releases/download/${version}/cops-${version}.zip";
+    sha256 = "sha256-aMnvs0CrMJic2JoZoXPEWHLTPthd0CZ0+K8It3SMvHc=";
+  };
+
+  nativeBuildInputs = [ unzip ];
+
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir $out
+    cp -r * $out/
+    cp ${configFile} $out/config_local.php
+  '';
+}
