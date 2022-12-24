e28469cf8e0ec2eaa373edde320b4be009914150
Lan Tian <xuyh0120@outlook.com>
2022-12-24 15:58:21 -0600

    general: exclude customized packages from CI

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -51,7 +51,7 @@ mkScope (self: pkg: rec {
   asteriskDigiumCodecs_20 = pkgs.recurseIntoAttrs (pkg ./asterisk-digium-codecs/20.nix { });
   asteriskDigiumCodecs = asteriskDigiumCodecs_19;
 
-  lantianCustomized = pkgs.recurseIntoAttrs {
+  lantianCustomized = ifNotCI (pkgs.recurseIntoAttrs {
     # Packages with significant customization by Lan Tian
     asterisk = pkg ./lantian-customized/asterisk { };
     coredns = pkg ./lantian-customized/coredns { };
@@ -61,12 +61,12 @@ mkScope (self: pkg: rec {
     linux-xanmod-lantian-lto-config = ifNotCI lantianCustomized.linux-xanmod-lantian-lto.configfile;
     nbfc-linux = pkg ./lantian-customized/nbfc-linux { };
     nginx = pkg ./lantian-customized/nginx { };
-  };
+  });
 
-  lantianPersonal = pkgs.recurseIntoAttrs {
+  lantianPersonal = ifNotCI (pkgs.recurseIntoAttrs {
     # Personal packages with no intention to be used by others
     libltnginx = pkg ./lantian-personal/libltnginx { };
-  };
+  });
 
   openj9-ibm-semeru = ifNotCI (pkgs.recurseIntoAttrs (pkg ./openj9-ibm-semeru { }));
   openjdk-adoptium = ifNotCI (pkgs.recurseIntoAttrs (pkg ./openjdk-adoptium { }));
