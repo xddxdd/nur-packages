648bfcf91b388e295e532270a21c82f72237f0f9
Lan Tian <xuyh0120@outlook.com>
2022-12-27 05:15:07 -0600

    linux-xanmod-lantian: alternative method for LTO flags

--- pkgs/lantian-customized/linux-xanmod-lantian/default.nix
+++ pkgs/lantian-customized/linux-xanmod-lantian/default.nix
@@ -11,23 +11,45 @@
 # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/kernel/linux-xanmod.nix
 let
   # https://github.com/NixOS/nixpkgs/pull/129806
+  # https://github.com/lovesegfault/nix-config/blob/master/nix/overlays/linux-lto.nix
   stdenvLLVM =
     let
-      llvmPin = pkgs.llvmPackages_latest.override {
-        bootBintools = null;
-        bootBintoolsNoLibc = null;
+      noBintools = { bootBintools = null; bootBintoolsNoLibc = null; };
+      hostLLVM = pkgs.pkgsBuildHost.llvmPackages_latest.override noBintools;
+      buildLLVM = pkgs.pkgsBuildBuild.llvmPackages_latest.override noBintools;
+
+      mkLLVMPlatform = platform: platform // {
+        linux-kernel = platform.linux-kernel // {
+          makeFlags = (platform.linux-kernel.makeFlags or [ ]) ++ [
+            "LLVM=1"
+            "LLVM_IAS=1"
+            "CC=${buildLLVM.clangUseLLVM}/bin/clang"
+            "LD=${buildLLVM.lld}/bin/ld.lld"
+            "HOSTLD=${hostLLVM.lld}/bin/ld.lld"
+            "AR=${buildLLVM.llvm}/bin/llvm-ar"
+            "HOSTAR=${hostLLVM.llvm}/bin/llvm-ar"
+            "NM=${buildLLVM.llvm}/bin/llvm-nm"
+            "STRIP=${buildLLVM.llvm}/bin/llvm-strip"
+            "OBJCOPY=${buildLLVM.llvm}/bin/llvm-objcopy"
+            "OBJDUMP=${buildLLVM.llvm}/bin/llvm-objdump"
+            "READELF=${buildLLVM.llvm}/bin/llvm-readelf"
+            "HOSTCC=${hostLLVM.clangUseLLVM}/bin/clang"
+            "HOSTCXX=${hostLLVM.clangUseLLVM}/bin/clang++"
+          ];
+        };
       };
 
-      stdenv' = pkgs.overrideCC llvmPin.stdenv llvmPin.clangUseLLVM;
+      stdenv' = pkgs.overrideCC hostLLVM.stdenv hostLLVM.clangUseLLVM;
     in
-    stdenv'.override {
-      extraNativeBuildInputs = [ llvmPin.lld pkgs.patchelf ];
-    };
+    stdenv'.override (old: {
+      hostPlatform = mkLLVMPlatform old.hostPlatform;
+      buildPlatform = mkLLVMPlatform old.buildPlatform;
+      extraNativeBuildInputs = [ hostLLVM.lld pkgs.patchelf ];
+    });
 in
 buildLinux {
   inherit lib;
   stdenv = if lto then stdenvLLVM else stdenv;
-  extraMakeFlags = lib.optionals lto [ "LLVM=1" "LLVM_IAS=1" ];
 
   inherit (sources.linux-xanmod) version src;
   modDirVersion =
@@ -42,11 +64,15 @@ buildLinux {
     let
       cfg = import ./config.nix args;
     in
-    if lto then
-      ((builtins.removeAttrs cfg [ "GCC_PLUGINS" "FORTIFY_SOURCE" ]) // (with lib.kernel; {
+    if !lto then cfg
+    else
+      ((builtins.removeAttrs cfg [
+        "GCC_PLUGINS"
+        "FORTIFY_SOURCE"
+      ]) // (with lib.kernel; {
         LTO_NONE = no;
         LTO_CLANG_FULL = yes;
-      })) else cfg;
+      }));
 
   kernelPatches = [
     pkgs.kernelPatches.bridge_stp_helper
@@ -58,5 +84,8 @@ buildLinux {
     })
     (builtins.attrNames (builtins.readDir ./patches)));
 
-  extraMeta.broken = !stdenv.hostPlatform.isx86_64;
+  extraMeta = {
+    description = "Linux Xanmod Kernel with Lan Tian Modifications" + lib.optionalString lto " and Clang+ThinLTO";
+    broken = !stdenv.isx86_64;
+  };
 }
