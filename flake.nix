54326f2ac751dde2944123a67414ae711c203d8e
Lan Tian <xuyh0120@outlook.com>
2023-06-01 00:23:51 -0700

    Revert "general: use system nvfetcher for now"
    This reverts commit 6d5383f4de8562b186c76d89693cbb44ca693c9f.

--- flake.nix
+++ flake.nix
@@ -104,7 +104,7 @@
 
             nvfetcher = ''
               [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="-k $HOME/Secrets/nvfetcher.toml" || KEY_FLAG=""
-              nvfetcher $KEY_FLAG -c nvfetcher.toml -o _sources "$@"
+              ${pkgs.nvfetcher}/bin/nvfetcher $KEY_FLAG -c nvfetcher.toml -o _sources "$@"
               ${readme}
             '';
 
