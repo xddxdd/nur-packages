45149c83b70fc65ff12a16e6ddf751fd0f7f2ddf
Lan Tian <xuyh0120@outlook.com>
2023-06-02 23:23:55 -0700

    general: fix nvfetcher PATH to nix-prefetch-git

--- flake.nix
+++ flake.nix
@@ -104,6 +104,7 @@
 
             nvfetcher = ''
               [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="-k $HOME/Secrets/nvfetcher.toml" || KEY_FLAG=""
+              export PATH=${pkgs.nix-prefetch-scripts}/bin:$PATH
               ${pkgs.nvfetcher}/bin/nvfetcher $KEY_FLAG -c nvfetcher.toml -o _sources "$@"
               ${readme}
             '';
