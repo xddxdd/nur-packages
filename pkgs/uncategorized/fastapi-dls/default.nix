866407e91a38fa132a801658dc0868d57d8bb18c
Lan Tian <xuyh0120@outlook.com>
2023-06-05 23:47:07 -0700

    Revert "fastapi-dls: chdir on startup"
    This reverts commit 7e52224604a72956388816c4be7b5a2da4ccecb3.

--- pkgs/uncategorized/fastapi-dls/default.nix
+++ pkgs/uncategorized/fastapi-dls/default.nix
@@ -20,7 +20,6 @@
     ]);
 
   name = with sources.fastapi-dls; "${pname}-${version}";
-  src = sources.fastapi-dls.src;
 in
   runCommand name {
     inherit (sources.fastapi-dls) pname version;
@@ -33,10 +32,9 @@ in
   } ''
     mkdir -p $out/bin
     makeWrapper ${python}/bin/python $out/bin/fastapi-dls \
-      --chdir "${src}/app" \
       --add-flags "-m" \
       --add-flags "uvicorn" \
       --add-flags "--app-dir" \
-      --add-flags "${src}/app" \
+      --add-flags "${sources.fastapi-dls.src}/app" \
       --add-flags "main:app" \
   ''
