2dd37f51a3781122c2579362c1f87ff22639cc54
Lan Tian <xuyh0120@outlook.com>
2024-03-02 05:21:20 -0800

    general: fix unbound PYTHONPATH variable

--- flake-modules/commands.nix
+++ flake-modules/commands.nix
@@ -35,7 +35,7 @@
         KEY_FLAG=""
         [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="$KEY_FLAG -k $HOME/Secrets/nvfetcher.toml"
         [ -f "secrets.toml" ] && KEY_FLAG="$KEY_FLAG -k secrets.toml"
-        export PYTHONPATH=${pkgs.python3Packages.packaging}/lib/python${pkgs.python3.pythonVersion}/site-packages:$PYTHONPATH
+        export PYTHONPATH=${pkgs.python3Packages.packaging}/lib/python${pkgs.python3.pythonVersion}/site-packages:''${PYTHONPATH:-}
         ${inputs'.nvfetcher.packages.default}/bin/nvfetcher $KEY_FLAG --keep-going -c nvfetcher.toml -o _sources "$@"
         ${readme}
       '';
