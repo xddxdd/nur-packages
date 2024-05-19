46985f4059192e1551bc3d62208906c6db0b6849
Lan Tian <xuyh0120@outlook.com>
2024-05-19 13:44:15 -0700

    general: handle edge case if nvfetcher sources is not a set

--- helpers/nvfetcher-loader.nix
+++ helpers/nvfetcher-loader.nix
@@ -4,23 +4,26 @@ let
   sources = pkgs.callPackage sourcesFile { };
 in
 lib.mapAttrs (
-  _n: v:
-  let
-    removeVersionPrefix = {
-      version = lib.removePrefix "v" v.version;
-    };
-    unstableDateVersion =
-      if
-        (builtins.hasAttr "version" v)
-        && (builtins.match "[0-9a-f]{40}" v.version != null)
-        && (builtins.hasAttr "date" v)
-      then
-        { version = "unstable-${v.date}"; }
-      else
-        { };
-  in
-  lib.foldl lib.recursiveUpdate v [
-    removeVersionPrefix
-    unstableDateVersion
-  ]
+  _: v:
+  if !(lib.isAttrs v) then
+    v
+  else
+    let
+      removeVersionPrefix = {
+        version = lib.removePrefix "v" v.version;
+      };
+      unstableDateVersion =
+        if
+          (builtins.hasAttr "version" v)
+          && (builtins.match "[0-9a-f]{40}" v.version != null)
+          && (builtins.hasAttr "date" v)
+        then
+          { version = "unstable-${v.date}"; }
+        else
+          { };
+    in
+    lib.foldl lib.recursiveUpdate v [
+      removeVersionPrefix
+      unstableDateVersion
+    ]
 ) sources
