0084f1cdf4b0a1d7831528bbb6a5f7789bb334af
Lan Tian <xuyh0120@outlook.com>
2025-04-23 23:06:55 -0700

    plangothic-fonts: keep old aliases working

--- pkgs/uncategorized/plangothic-fonts/default.nix
+++ pkgs/uncategorized/plangothic-fonts/default.nix
@@ -4,23 +4,30 @@
   sources,
   ...
 }:
-stdenvNoCC.mkDerivation rec {
-  pname = "plangothic-fonts";
-  inherit (sources.plangothic-fonts) version src;
-  installPhase = ''
-    runHook preInstall
+let
+  package = stdenvNoCC.mkDerivation rec {
+    pname = "plangothic-fonts";
+    inherit (sources.plangothic-fonts) version src;
+    installPhase = ''
+      runHook preInstall
 
-    mkdir -p $out/share/fonts/truetype/
-    cp "fonts/static/PlangothicP1-Regular.ttf" $out/share/fonts/truetype/
-    cp "fonts/static/PlangothicP2-Regular.ttf" $out/share/fonts/truetype/
+      mkdir -p $out/share/fonts/truetype/
+      cp "fonts/static/PlangothicP1-Regular.ttf" $out/share/fonts/truetype/
+      cp "fonts/static/PlangothicP2-Regular.ttf" $out/share/fonts/truetype/
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  meta = {
-    maintainers = with lib.maintainers; [ xddxdd ];
-    description = "Plangothic Project";
-    homepage = "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic_Project";
-    license = lib.licenses.ofl;
+    meta = {
+      maintainers = with lib.maintainers; [ xddxdd ];
+      description = "Plangothic Project";
+      homepage = "https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic_Project";
+      license = lib.licenses.ofl;
+    };
   };
+in
+package
+// {
+  allideo = package;
+  fallback = package;
 }
